9a8170cb1c1819673a2f2c5c635e5e1aeae842da
Tertullien20 <tertulliendeguenon@gmail.com>
2024-01-09 21:40:37 +0100

    e

--- lib/file_test.dart
+++ lib/file_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'provider/language/app_localizations.dart';
-import 'provider/language/language_provider.dart';
+import '../../provider/language/app_localizations.dart';
+import '../../provider/language/language_provider.dart';
 
 class Splash extends StatefulWidget {
 
@@ -48,7 +47,7 @@ class _SplashState extends State<Splash> {
                             Navigator.pop(context);
                           }
                         },
-                        items: const [
+                        items: [
                           DropdownMenuItem(
                             child: Text("English"),
                             value: Locale("en", "US"),
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:solution/firebase_options.dart';
 import 'package:solution/view/apps/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'provider/language/language_provider.dart';
+import 'utils/constants/colors.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The Solution Web Mobile Application Open Source Project Competition Rules and Guidelines',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: primary),
         useMaterial3: true,
       ),
       locale: context.watch<LanguageProvider>().locale,
