4505c6d92a62fb3f5d57c0dbdd8b1b7c406eefb0
Tertullien20 <tertulliendeguenon@gmail.com>
2024-01-10 01:03:26 +0100

    e

--- lib/view/widgets/input.dart
+++ lib/view/widgets/input.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:solution/utils/constants/colors.dart';
 
-
-Widget input(TextEditingController controller, {decoration, bool obscureText = false, TextInputType keyboardType = TextInputType.text, List<String? Function(String?)>? validators, int? maxLines, onChanged, bool enabled = true, colorBorder = secondary }) {
-  return
-    Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(7.0),
-          border: Border.all(
-            color: colorBorder,
-          ),
-        ),
-        child:  TextFormField(
-          enabled: enabled,
-          decoration:decoration,
-          obscureText: obscureText,
-          keyboardType: keyboardType,
-          validator: (val) {
-            if (validators != null) {
-              for (var e in validators) {
-                final res = e(val);
-                if (res != null) return res;
-              }
+Widget input(TextEditingController controller,
+    {decoration,
+    bool obscureText = false,
+    TextInputType keyboardType = TextInputType.text,
+    List<String? Function(String?)>? validators,
+    int? maxLines,
+    onChanged,
+    bool enabled = true,
+    colorBorder = secondary}) {
+  return Container(
+      decoration: BoxDecoration(
+        //color: white,
+        borderRadius: BorderRadius.circular(7.0),
+        border: Border.all(color: colorBorder, width: 2.0),
+      ),
+      child: TextFormField(
+        enabled: enabled,
+        decoration: decoration,
+        obscureText: obscureText,
+        keyboardType: keyboardType,
+        validator: (val) {
+          if (validators != null) {
+            for (var e in validators) {
+              final res = e(val);
+              if (res != null) return res;
             }
-            return null;
-          },
-          onChanged: onChanged,
-          maxLines: obscureText ? 1 : maxLines,
-        ));
+          }
+          return null;
+        },
+        onChanged: onChanged,
+        maxLines: obscureText ? 1 : maxLines,
+      ));
 }
+
 InputDecoration textFieldDecoration(String hintText,
     {String labelText = "",
-      Color colorHint = white,
-      String? errorText,
-      bool filled = true,
-      Widget? prefixIcon,
-      Widget? suffixIcon}) {
+    Color colorHint = white,
+    String? errorText,
+    bool filled = true,
+    Widget? prefixIcon,
+    Widget? suffixIcon}) {
   if (labelText == "") {
     labelText = hintText;
   }
@@ -58,6 +64,6 @@ InputDecoration textFieldDecoration(String hintText,
 
 InputBorder textFieldBorder() {
   return const OutlineInputBorder(
-     borderSide: BorderSide(width: 2.0),
+      borderSide: BorderSide(width: 2.0),
       borderRadius: BorderRadius.all(Radius.circular(8.0)));
 }
