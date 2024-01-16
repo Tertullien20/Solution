d08c3875d4d3eb29379edc390c28f6796d261be6
Tertullien20 <tertulliendeguenon@gmail.com>
2024-01-16 23:38:23 +0100

    e

---
+++ lib/view/apps/profile/app_bar_profile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/text.dart';
+
+
+class MyProfileAppBar extends StatelessWidget {
+  const MyProfileAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlexibleSpaceBar(
+      expandedTitleScale: 2.0,
+      centerTitle: true,
+      background: Image.asset(
+        'img/others/background_app.png',
+        fit: BoxFit.cover,
+        height: 100,
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0, top: 20.0, bottom: 20.0),
+            child: Container(
+              alignment: Alignment.topRight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(Icons.arrow_circle_left_outlined)),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 60.0, left: 20.0, bottom: 15.0),
+                    child: text("Mon compte", size: 28.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/apps/profile/my_account.dart
@@ -0,0 +1,5 @@
+/*appBar: AppBar(
+toolbarHeight: 180.0,
+automaticallyImplyLeading: false,
+flexibleSpace: const MyProfileAppBar(),
+),*/
